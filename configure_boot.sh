6f222e187e60316650792971bb9ef032662de54d
Mike Souza <contact@michael-souza.com>
2018-04-21 11:36:55 -0400

    Add support to disable onboard Bluetooth and WiFi

--- configure_boot.sh
+++ configure_boot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-usage="Usage: $0 mount_point ip_address gateway netmask hostname interface"
+usage="Usage: $0 mount_point ip_address gateway netmask hostname interface disable_bt disable_wifi"
 if [ "$#" == "0" ]; then
 	echo "${usage}"
 	exit 1
@@ -17,6 +17,8 @@ gateway=${3:-"192.168.1.1"}
 subnet=${4:-"255.255.0.0"}
 hostname=${5:-"rpi0"}
 interface=${6:-"eth0"}
+disable_bt=${7:-false}
+disable_wifi=${8:-false}
 
 if [ "$#" == "6" ]; then
 	autoconf=off
@@ -37,3 +39,14 @@ if [ -e wpa_supplicant.conf ]; then
 	echo "Configuring Wi-Fi..."
 	cp wpa_supplicant.conf "${mount_point}/"
 fi
+
+if [ "${disable_bt}" = true ]; then
+	echo "Disabling Bluetooth..."
+	echo -e "\n# Disable onboard Bluetooth\ndtoverlay=pi3-disable-bt" >> "${mount_point}/config.txt"
+fi
+
+if [ "${disable_wifi}" = true ]; then
+	echo "Disabling WiFi..."
+	echo -e "\n# Disable onboard WiFi\ndtoverlay=pi3-disable-wifi" >> "${mount_point}/config.txt"
+fi
+
--- setup_rpi.sh
+++ setup_rpi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-usage="Usage: $0 image_file [ip_address] [gateway] [netmask] [hostname] [interface]"
+usage="Usage: $0 image_file [ip_address] [gateway] [netmask] [hostname] [interface] [disable_bt] [disable_wifi]"
 if [ "$#" == "0" ]; then
 	echo "${usage}"
 	exit 1
